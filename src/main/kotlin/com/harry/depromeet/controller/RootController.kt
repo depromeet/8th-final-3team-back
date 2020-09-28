85d4bba0569a028ed6bdefccf518287d31b8600b
harry(조민국) <harry.code@kakaoenterprise.com>
2020-09-28 20:06:43 +0900

    init

---
+++ src/main/kotlin/com/harry/depromeet/controller/RootController.kt
@@ -0,0 +1,13 @@
+package com.harry.depromeet.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class RootController {
+
+    @GetMapping("/")
+    fun root(): String {
+        return "Hello"
+    }
+}
---
+++ src/test/kotlin/com/harry/depromeet/DepromeetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.harry.depromeet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DepromeetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
