82e670d40582b9ce8233828069dfac85fae9f82c
minkukjo <strike0115@naver.com>
2020-12-01 10:22:24 +0900

    RestTemplate 옵션에 인가된 서비스로 변경

--- src/main/kotlin/com/harry/depromeet/service/PlaceService.kt
+++ src/main/kotlin/com/harry/depromeet/service/PlaceService.kt
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.harry.depromeet.model.Place
 import com.harry.depromeet.utils.JsonUtils
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
@@ -12,11 +15,18 @@ import java.lang.NullPointerException
 import java.lang.RuntimeException
 
 @Service
-class PlaceService(val restTemplate: RestTemplate) {
+class PlaceService {
 
     fun getPlace(placeId: Long): Place {
+        val restTemplate = RestTemplate()
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.valueOf("text/plain;charset=utf-8");
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)" +
+                " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        val entity = HttpEntity<String>("", headers)
+
         val url = "https://place.map.kakao.com/main/v/$placeId"
-        val response = restTemplate.exchange(url, HttpMethod.GET, HttpEntity.EMPTY, String::class.java)
+        val response = restTemplate.exchange(url, HttpMethod.GET, entity, String::class.java)
         response.body?.let {
             return toPlace(it)
         } ?: throw NullPointerException()
